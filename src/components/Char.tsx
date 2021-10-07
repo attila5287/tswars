546caaa536d6216d09193fcbfce6caf2de3a2de8
attila5287 <atiturkoz@hotmail.com>
2021-10-07 03:35:08 -0600

    a tag on films

--- src/components/Char.tsx
+++ src/components/Char.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
-import { CharType } from "../types";
+import { CharType, FilmType } from "../types";
+import helpers from "../utils/helpers";
 
 interface ICharProps {
   char: CharType;
@@ -7,25 +8,25 @@ interface ICharProps {
 
 const Char: FunctionComponent<ICharProps> = (props) => {
   const { char } = props;
-
+ 
     return (
       <ul>
-    <li> name : {char.name}</li>
-    <li> height : {char.height}</li>
-    <li> mass : {char.mass}</li>
-    <li> hair_color : {char.hair_color}</li>
-    <li> skin_color : {char.skin_color}</li>
-    <li> eye_color : {char.eye_color}</li>
-    <li> birth_year : {char.birth_year}</li>
-    <li> gender : {char.gender}</li>
-    <li> homeworld : {char.homeworld}</li>
-    <li> films : {char.films}</li>
-    <li> species : {char.species}</li>
-    <li> vehicles : {char.vehicles}</li>
-    <li> starships : {char.starships}</li>
-    <li> created : {char.created}</li>
-    <li> edited : {char.edited}</li>
-    <li> url : {char.url}</li>
+        <li> name : {char.name}</li>
+        <li> height : {char.height}</li>
+        <li> mass : {char.mass}</li>
+        <li> hair_color : {char.hair_color}</li>
+        <li> skin_color : {char.skin_color}</li> 
+        <li> eye_color : {char.eye_color}</li>
+        <li> birth_year : {char.birth_year}</li>
+        <li> gender : {char.gender}</li>
+        <li> homeworld : { char.homeworld }</li>
+        <li> films :{ char.films.map(f=>(<p key={f}><a href={f}> { f }</a></p>))}</li>
+        <li> species : { char.species.map(s=>(<p key={s}><a href={s}>{ s }</a></p>)) }</li>
+        <li> vehicles : { char.vehicles.map(v=>(<p key={v}><a href={v}>{ v }</a></p>))}</li>
+        <li> starships : { char.starships.map(s=>(<p key={s}><a href={s}>{ s }</a></p>)) }</li>
+        <li> created : {helpers.formatDate(char.created)}</li>
+        <li> edited : {helpers.formatDate(char.edited)}</li>
+        <li> url : <a href={char.url}>{char.url}</a> </li>
       </ul>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,42 @@
+export interface FilmType {
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    characters: string[];
+    planets: string[];
+    starships: string[];
+    vehicles: string[];
+    species: string[];
+    created: Date;
+    edited: Date;
+    url: string;
+}
 
-    export interface CharType {
-        name: string;
-        height: string;
-        mass: string;
-        hair_color: string;
-        skin_color: string;
-        eye_color: string;
-        birth_year: string;
-        gender: string;
-        homeworld: string;
-        films: string[];
-        species: any[];
-        vehicles: string[];
-        starships: string[];
-        created: Date;
-        edited: Date;
-        url: string;
-    }
+export interface CharType {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: any[];
+    vehicles: string[];
+    starships: string[];
+    created: Date;
+    edited: Date;
+    url: string;
+}
 
-    export interface Results {
-        count: number;
-        next?: any;
-        previous?: any;
-        results: CharType[];
-    }
+export interface Results {
+    count: number;
+    next?: any;
+    previous?: any;
+    results: CharType[];
+}
