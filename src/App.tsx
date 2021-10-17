0f680058e44e73196c6fb20e100e77472b7e21e1
attila5287 <atiturkoz@hotmail.com>
2021-10-17 06:17:22 -0600

    b4 select api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Char from "./components/Char";
 import { Results, CharType } from "./types";
 import Loading from "./components/Loading";
+import {Animated} from 'react-animated-css';
+import "./index.css";
 
 function App() {
   const [chars, setChars] = useState<CharType[]>([]);
@@ -37,24 +39,38 @@ function App() {
   }, []);
 
   return (
-    <div className='container pt-2'>
-          <Loading loading={ loading }>
-              <h1 className='fas fa-search mx-2'></h1>
-              <select className='form-select' onChange={ (event) => handleInputChange(event) }>
+<>              <nav className="navbar navbar-expand navbar-dark bg-primary">
+                <div className="btn btn-outline-info disabled my-2 my-sm-0 btn-lg text-nowrap">
+              <i className='fab fa-rebel mx-2'></i>
+              <i className='fab fa-empire mx-2'></i>
+                </div>                  
+              <select className='form-control' onChange={ (event) => handleInputChange(event) }>
                   <option value="" key="0" hidden>...</option>
                   <option value="anakin" key="1">anakin</option>
                   <option value="luke" key="2">luke</option>
           </select>
+                  
+              </nav>
+    <div className='container pt-2'>
+          <Loading loading={ loading }>
+
         {chars.map((char,i) => {
             return (
-                <div key={i}>
-                    <br />
+                              <Animated
+				animationIn='bounceInLeft'
+				animationOut='bounceInLeft'
+				isVisible={true}
+                    animationInDelay={ 500 }
+                        key={ i }
+                    
+              >
                 <Char key={ char.name } char={ char } />
-                </div>
+                    
+              </Animated>
             );
         }) }
       </Loading>
-    </div>
+    </div></>
   );
 }
 
