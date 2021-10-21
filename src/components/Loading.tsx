3985716e8419f818a2bd6a857fdd97aba59bd25b
attila5287 <atiturkoz@hotmail.com>
2021-10-21 05:30:14 -0600

    nav redesgn

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from "react";
+import {Animated} from 'react-animated-css';
+
 
 interface ILoadingProps {
   loading: boolean;
@@ -8,7 +10,18 @@ const Loading: FunctionComponent<ILoadingProps> = (props) => {
   const { loading } = props;
 
   if (loading) {
-    return <>Loading...</>;
+      return <div className=''>
+          <h1 className='fas fa-spinner fa-pulse mx-2'>
+          </h1>
+          <Animated
+            animationIn='fadeIn'
+            animationOut='bounceOut'
+            isVisible={true}
+            ></Animated>
+          <h1>
+          Loading...
+          </h1>
+      </div>;
   }
 
   return <>{props.children}</>;
